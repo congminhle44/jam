34353d83b0c69c072c172d2838776390d27b1ca5
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2021-06-22 16:34:40 +0700

    feat: support client through chat

--- src/routes/layout/client.js
+++ src/routes/layout/client.js
@@ -1,9 +1,18 @@
 /** @format */
-import Footer from '../components/Footer';
+import { useEffect } from 'react';
+import tawkTo from 'tawkto-react';
 
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 const ClientLayout = ({ children }) => {
+  const tawkToPropertyId = '60111887c31c9117cb72e68c';
+  const tawkToKey = '1et1cfl5v';
+
+  useEffect(() => {
+    tawkTo(tawkToPropertyId, tawkToKey);
+  }, []);
+
   return (
     <div>
       <Header />
